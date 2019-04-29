bae9ee112e79afb380491c9428b7caa0188ef687
Nick Potiriadis <32678324+NickyPot@users.noreply.github.com>
2019-04-29 11:33:13 +0200

    mrna and coding strand working together

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -23,12 +23,12 @@ vector <string> protein;
 
 
 //this will run at the same time as the findMRna function
-string findCodingStrand(string templateStrand, int iteration, int numberOfBases, int whichThread) {
+string findCodingStrand(string templateStrand, int iteration, int numberOfBases) {
 
 	//this is only used within the thread function, as a stepping stone to the global variable
 	vector <char> threadedTemplateStrand;
-	
-	
+
+
 	//goes through coding strand and turns it into the template strand
 	for (; iteration < numberOfBases; iteration++)
 	{
@@ -44,14 +44,14 @@ string findCodingStrand(string templateStrand, int iteration, int numberOfBases,
 			break;
 
 		}
-		
+
 
 	}
-	
+
 	//turns the vector char template strand into a string, in order to put it in the global string array for the template strand
 	string threadedTempStrand(threadedTemplateStrand.begin(), threadedTemplateStrand.end());
 
-	
+
 
 	return threadedTempStrand;
 }
@@ -62,10 +62,10 @@ string findCodingStrand(string templateStrand, int iteration, int numberOfBases,
 string findMRna(string templateStrand, int iteration, int numberOfBases) {
 
 	vector <char> threadedMRna;
-	
+
 
 	//goes through template strand and turns it into mRNA
-	for ( ; iteration < numberOfBases; iteration++)
+	for (; iteration < numberOfBases; iteration++)
 	{
 		switch (templateStrand[iteration])
 		{
@@ -79,7 +79,7 @@ string findMRna(string templateStrand, int iteration, int numberOfBases) {
 			break;
 
 		}
-			   		 
+
 	}
 
 	string threadedMRnaInString(threadedMRna.begin(), threadedMRna.end());
@@ -99,14 +99,14 @@ int main() {
 	string dnaStrand;
 	string dnaStrand2;
 	while (getline(dnaFile, dnaStrand)) {
-		
-		
+
+
 		dnaStrand2 = dnaStrand2 + dnaStrand;
-		
+
 	}
 
 
-	
+
 	//allows for easy performance evaluation
 	int numberOfThreads;
 	cout << "what is the number of threads? ";
@@ -116,41 +116,64 @@ int main() {
 	int maxBase = 0;
 	int codingBases = dnaStrand2.length();
 	int basesJump = codingBases / numberOfThreads;
-	
+
 	//async object that gets the coding strands from the threads
 	vector <future<string>> codingStrandsFromThreads;
-	
+	vector <future<string>> mRnaStrandsFromThreads;
+
+	//this is where the problem is
 	for (int i = 0; i < numberOfThreads; i++) {
-	
+
 		maxBase = maxBase + basesJump;
 		int startingPos = basesJump * i;
-		codingStrandsFromThreads.push_back(async(findMRna, dnaStrand2, startingPos, maxBase));
-	
-	
+		mRnaStrandsFromThreads.push_back(async(findMRna, dnaStrand2, startingPos, maxBase));
+
+
+	}
+
+	maxBase = 0;
+
+	for (int i = 0; i < numberOfThreads; i++) {
+
+		maxBase = maxBase + basesJump;
+		int startingPos = basesJump * i;
+		codingStrandsFromThreads.push_back(async(findCodingStrand, dnaStrand2, startingPos, maxBase));
+
+
 	}
 
 	fstream dnaOutput("dnaOut.txt");
+
+	dnaOutput << "This is the mrna strand: ";
+	for (int i = 0; i < numberOfThreads; i++)
+	{
+		dnaOutput << mRnaStrandsFromThreads[i].get();
+
+	}
+	dnaOutput << "\n \n";
+
 	
+	dnaOutput << "This is the coding strand: ";
 	for (int i = 0; i < numberOfThreads; i++)
 	{
 		dnaOutput << codingStrandsFromThreads[i].get();
 
 	}
-	
+
 	
 	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<microseconds>(stop - start);
 
-	
+
 	cout << duration.count();
 
 	//cout << fff;
 
-	
-	
 
 
-	
+
+
+
 
 
 
